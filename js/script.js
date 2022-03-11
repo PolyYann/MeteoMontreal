44a8f5e6368fdf7c6bf27d34b434e342b4a5dca2
chricrie <9568730.bdeb.qc.ca>
2022-03-11 07:43:57 -0500

    Commit - formatage des dates

--- js/script.js
+++ js/script.js
@@ -19,50 +19,89 @@ function ajoutCard(nbrJour) {
             return reponse.json();
         })
         .then(function (data) {
-            
+            //Table de la DB      
             var donneeTemp = data.temperatures;
-            let parent = document.getElementById("parent")
+            //Index DB de la journee courante
+            let indexJour = indexJourneeCourante()
+            //Tableau d'objet nom=carte
+            let tableCarte = document.getElementsByName("carte")
+            
+            //parent
+           // let parent = document.getElementById("parent")
             //carousel
-            let carousel = document.createElement("div")
-            carousel.setAttribute("class", "bg-secondary owl-carousel owl-theme container-sm")
+            //let carousel = document.getElementById("parent")
+            //carousel.setAttribute("class", "bg-secondary owl-carousel owl-theme container-sm")
             //creer cartes
-            for (let index = 0; index <= nbrJour; index++) {
+           
+            
+            for (let index = indexJour+1; index < (indexJour + tableCarte.length); index++) {
+                //référence de la carte modifié chaque ittération
+                let card = tableCarte[index-indexJour]
                 //carte parent
-                let card = document.createElement("div")
-                card.setAttribute("class", "card")
+                //let card = document.createElement("div")
+                //card.setAttribute("class", "card")
                 
                 //ajouter date
+                let weekDay = formaterDate(donneeTemp[index].DateDuJour)
                 let divDay = document.createElement("div")
-                divDay.setAttribute("class", "h2 card-header bg-dark text-light text-center")
-                
-                divDay.innerHTML = donneeTemp[index].DateDuJour
+                divDay.setAttribute("class", "h5 card-header bg-dark text-light text-center")
+                divDay.innerHTML = weekDay
 
-                //ajouter Max Temperature
+                // //ajouter Max Temperature
                 let divMax = document.createElement("div")
                 divMax.setAttribute("class", "h3 bg-warning text-danger text-center")
                 divMax.innerHTML = donneeTemp[index].MaxTemp
-                //ajouter Minumum temperature
+                // //ajouter Minumum temperature
                 let divMin = document.createElement("div")
                 divMin.setAttribute("class", "h3 bg-info text-light text-center")
                 divMin.innerHTML = donneeTemp[index].MinTemp
-                //ajouter Image
+                // //ajouter Image
                 let divImage = document.createElement("div")
-                divImage.setAttribute("class", "card-footer bg-light ")
+                divImage.setAttribute("class", "card-footer bg-light")
+                divImage.setAttribute("style", "width:auto")
                 let image= document.createElement("img")
                  image.setAttribute("src", getImage(donneeTemp[index].Temp))
                 divImage.appendChild(image)
-                //
+            card.setAttribute("style", "width:150px height")
+                //carousel.appendChild(card)
                 card.appendChild(divDay)
                 card.appendChild(divMax)
                 card.appendChild(divMin)
-                card.appendChild(divImage)
-                carousel.appendChild(card)
-                
+                card.appendChild(divImage)   
             }
-            parent.appendChild(carousel)
+            //parent.appendChild(carousel)
         });
 }
 
+function ajoutPrevisionJournaliere() {
+    //fetch DB des températures journalières
+    fetch('../data/temperatures_2022.json')
+        .then(function (reponse) {
+            return reponse.json();
+        })
+        .then(function (data) {
+            var donneeTemp = data.temperatures;
+            var jour = document.getElementById("journalierHeader")
+            var temperature = document.getElementById("journalierNow")
+            var tempMax = document.getElementById("journalierMax")
+            var tempMin = document.getElementById("journalierMin")
+            // index DB de la journee courante
+            let indexJour = indexJourneeCourante()
+            console.log(indexJour)
+            //formatage affichage de la journée
+            let weekDay = formaterDate(donneeTemp[indexJour].DateDuJour)
+            jour.innerText = weekDay
+            jour.setAttribute("class", "h2 bg-dark text-center text-light")
+            temperature.textContent = ("Maintenant: " +donneeTemp[indexJour].Temp)
+            temperature.setAttribute("class", "h3 bg-info text-center text-light") 
+            tempMax.innerText = ("Max: " + donneeTemp[indexJour].MaxTemp)
+            tempMax.setAttribute("class", "h3 bg-warning text-center text-danger") 
+            tempMin.innerText = ("Min: " + donneeTemp[indexJour].MinTemp)
+            tempMin.setAttribute("class", "h3 bg-info text-center text-light") 
+        });                
+}
+
+//Retour d'une image en fonction de la température recu en argument
 function getImage(temp) {
     if (temp <= 0){
        return "../images/neige.png"
@@ -74,40 +113,21 @@ function getImage(temp) {
         return "../images/soleil.png"
     }
 }
-
-function ajoutPrevisionJournaliere() {
-    //fetch DB
-    fetch('../data/temperatures_2022.json')
-        .then(function (reponse) {
-            return reponse.json();
-        })
-        .then(function (data) {
-                     
-
-            var TempJour = data.temperatures;
-            var jour = document.getElementById("journalierHeader")
-            var temperature = document.getElementById("journalierNow")
-            var tempMax = document.getElementById("journalierMax")
-            var tempMin = document.getElementById("journalierMin")
-            jour.value
-            jour.innerText = TempJour[0].DateDuJour
-            jour.setAttribute("class", "h2 bg-dark text-center text-light")
-            temperature.textContent = ("Horaire: " +TempJour[0].Temp)
-            temperature.setAttribute("class", "h3 bg-info text-center text-light") 
-            tempMax.innerText = ("Max: " + TempJour[0].MaxTemp)
-            tempMax.setAttribute("class", "h3 bg-warning text-center text-danger") 
-            tempMin.innerText = ("Min: " + TempJour[0].MinTemp)
-            tempMin.setAttribute("class", "h3 bg-info text-center text-light") 
-        });
+//Retour de la date en format d'affichage du site
+function formaterDate(tmpDate){
+    return date = new Date(tmpDate).toLocaleString
+        ('fr-CA', {  weekday: 'long', day: '2-digit' });
+}
+//Retour d'un index indiquant la journéé courrant en jour de l'annee
+function indexJourneeCourante(){
+    var date = new Date();
+    var nouvelleAns = new Date(date.getFullYear(), 0, 0);
+    var difference = date - nouvelleAns;
+    var journee = 1000 * 60 * 60 * 24;
+    var jour = Math.floor(difference / journee);
+    return jour;
 }
 
 ajoutPrevisionJournaliere()
-
-function trouveJourneeCourante(){
-    
-}
-
-
-
 
 
